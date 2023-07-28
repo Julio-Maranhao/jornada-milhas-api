1ece3022fd13f39755cc315f7cc06a6fac195164
Julio Maranhao <julio.maranhao@outlook.com>
2023-07-28 20:34:38 -0300

    add migrations

---
+++ src/db/data-source.config.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+
+config();
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_NAME'),
+  entities: [__dirname + '/../**/*.entity{.js,.ts}'],
+  migrations: [__dirname + '/migrations/*{.js,.ts}'],
+});
